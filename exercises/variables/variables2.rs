8dd4e1f1aa74d7a996b30e7d65a61071ecf522d2
Muhammed Abdel Hamid Shelleh <muhammedshelleh@gmail.com>
2024-03-20 00:41:04 -0400

    hehe

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -5,8 +5,8 @@
 
 // I AM NOT DONE
 
-fn main() {
-    let x;
+fn main(){
+    let x = 10;
     if x == 10 {
         println!("x is ten!");
     } else {
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -8,6 +8,6 @@
 fn main() {
     let x = 3;
     println!("Number {}", x);
-    x = 5; // don't change this line
+    let x = 5; // don't change this line
     println!("Number {}", x);
 }
